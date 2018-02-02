d339637d5cc3339e3beba9486a057722f6b713da
gaohuijue <gaohuijue@shinow.com>
2018-02-02 17:11:19 +0800

    增加timer演示。

--- Ffi_Test/Ffi_Test.h
+++ Ffi_Test/Ffi_Test.h
@@ -12,3 +12,4 @@ typedef int(*Listener)(char* name, char* code);
 extern "C" FFITEST_API wchar_t * say_hello();
 extern "C" FFITEST_API void register_listener(Listener listener);
 extern "C" FFITEST_API void fire();
+extern "C" FFITEST_API void start_timer();
